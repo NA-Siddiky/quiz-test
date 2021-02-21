c35baba4581f315f7d32b525865dfaa7249b657c
NA-Siddiky <siddiky.academic@gmail.com>
2021-02-22 00:50:09 +0600

    adding function

--- script.js
+++ script.js
@@ -1,5 +1,33 @@
 const searchItem = document.getElementById("search-item")
 const searchBtn = document.getElementById("search-btn")
+const url = "https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple"
 
-const 
 
+
+fetch(url)
+    .then(response => response.json())
+    .then(data => console.log(data));
+
+
+
+
+// searchBtn.addEventListener("clicked", function(){
+//     fetch(url)
+//     .then(response => response.json())
+//     .then(data => console.log(data));
+
+
+
+//     // console.log("clicked");  
+// })
+
+
+// const searchQuiz = () =>{
+//     fetch(url)
+//     .then(response => response.json())
+//     .then(data => console.log(data));
+
+
+
+//     console.log("clicked");
+// }
